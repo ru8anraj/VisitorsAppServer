9065d9b909f23c49281517af63e15bdd5de7028f
ru8anraj <ru8anraj@gmail.com>
2018-04-20 12:00:23 +0530

    added feel count

--- routes/logs.route.js
+++ routes/logs.route.js
@@ -15,6 +15,19 @@ logs.post('/setLog', (req, res) => {
     feel: req.body.feel,
     comment: req.body.comment
   }
+  var obj1 = {};
+  if (req.body.feel === 1) {
+    obj1 = {"feel.1": 1};
+  } else if (req.body.feel === 2) {
+    obj1 = {"feel.2": 1};
+  } else if (req.body.feel === 3) {
+    obj1 = {"feel.3": 1};
+  } else if (req.body.feel === 4) {
+    obj1 = {"feel.4": 1};
+  } else if (req.body.feel === 5) {
+    obj1 = {"feel.5": 1};
+  }
+
   MongoClient.connect('mongodb://localhost:27017', (err, client) => {
     const db = client.db('visitorLogs');
     db.collection('logs').insert(obj, (err, reply) => {
@@ -22,7 +35,13 @@ logs.post('/setLog', (req, res) => {
         console.log('Error in saving - > ', err);
       } else {
         console.log('reply - > ', reply);
-        res.send('OK');
+        db.collection('feelCount').update({key: 'feelCount'}, {$inc: obj1}, {upsert: true}, (err, rep) => {
+          if (err) {
+            console.log('Error in updating feel count - > ', err);
+          } else {
+            res.send('OK');
+          }
+        });
       }
     });
   });
@@ -33,10 +52,7 @@ logs.get('/getLog', (req, res) => {
     const db = client.db('visitorLogs');
     db.collection('logs').find().toArray((err, docs) => {
       console.log('docs - > ', docs);
-      var invertedDoc = [];
-      docs.map((item) => {
-        invertedDoc.unshift(item);
-      });
+      var invertedDoc = docs.reverse();
       res.json(invertedDoc);
     });
   });
